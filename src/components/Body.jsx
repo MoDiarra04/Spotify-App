39f195447ad6a6449624efc9f21ddefba5631c5c
Moritz Ternes <moritzternes@gmail.com>
2024-02-10 15:04:13 +0100

    nvabar change

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,19 +3,22 @@ import {useGlobalContext} from './Context'
 import { AppContext } from './Context'
 import MainPage from './mainpage'
 import Navbar from './Navbar'
+import { Box } from '@mui/material'
 
 const Body = () => {
   const {artist,setArtist} = useGlobalContext()
   if(artist !== undefined){
     console.log(artist)
     return (
-      <>
+      <Box sx={{ m: -1 }}>
         <Navbar/>
-        <MainPage/>
-        <img src={artist.images[2].url} alt="" />
-        <div>{artist.name}</div>
-        <div>{artist.followers.total}</div>
-      </>
+        <Box sx={{ m: 2 }}>
+          <MainPage/>
+          <img src={artist.images[2].url} alt="" />
+          <div>{artist.name}</div>
+          <div>{artist.followers.total}</div>
+        </Box>
+      </Box>
     )
   }
 }
