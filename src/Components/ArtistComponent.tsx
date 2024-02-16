18227e3ca4af995a9f5c370166a828dc85e4e17b
Modibo Diarra <modiarra04@gmail.com>
2024-02-16 17:03:56 +0100

    working on table

--- src/Components/ArtistComponent.tsx
+++ src/Components/ArtistComponent.tsx
@@ -1,11 +1,28 @@
+import * as React from 'react';
 import { useParams } from "react-router-dom";
-import { Tooltip, Typography, Container, Stack, Grid, Link } from "@mui/material";
+import { Tooltip, Typography, Container, Stack, Grid, Link, Modal, Fade, Button, Backdrop } from "@mui/material";
 import { getArtist, getArtist_TopTracks } from "../Utils/fetch";
 import { useEffect, useState } from "react";
 import { Box } from "@mui/system";
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import AlbumIcon from '@mui/icons-material/Album';
 import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+
+interface TableType{
+  album: string,
+  song:string
+}
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+};
 
 export const ArtistComponent = () => {
   const { id } = useParams();
@@ -18,7 +35,6 @@ export const ArtistComponent = () => {
       })
     }
   }, [])
-  console.log(artist)
   useEffect(() => {
     if (id !== undefined) {
       getArtist_TopTracks(id).then((res) => {
@@ -32,20 +48,68 @@ export const ArtistComponent = () => {
       console.log(topTrack.tracks)
     }
   }, [topTrack])
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [row,setRow] = useState<Array<TableType>>([])
+  useEffect(()=>{
+    if(topTrack !== undefined)
+    setRow(topTrack.tracks.map((item:any)=>{
+      return {album: item.album.name, song: item.name}
+    }))
+    console.log(row)
+  },[topTrack])
+  
   return (
     <Container maxWidth="xl">
-      <Stack direction="column" spacing={5} useFlexGap>
-        <Typography variant="h2" textAlign="center">
-          {artist ? artist?.name : ""}
-        </Typography>
+      <Typography variant="h2" textAlign="center">
+        {artist ? artist?.name : ""}
+      </Typography>
+      <a href={`https://open.spotify.com/search/${artist?artist?.name:""}`}>
+        <img style={{width:'100px'}} 
+          src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="" />
+      </a>
+      <Stack direction={{xs: 'column', sm: 'row'}}
+        justifyContent="center"
+        alignItems="center"
+        spacing={2} marginTop='30px'
+      >
         <Box sx={{ height: '320px' }}>
-          <img style={{ borderRadius: '170px' }} src={artist ? artist?.images[1].url : ""} alt="" />
+          <Button onClick={handleOpen}>
+            <img style={{ borderRadius: '170px' }} src={artist ? artist?.images[1].url : ""} alt="" />
+          </Button>
         </Box>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          slots={{ backdrop: Backdrop }}
+          slotProps={{
+            backdrop: {
+              timeout: 500,
+            },
+          }}
+        >
+          <Fade in={open}>
+            <Box sx={style}>
+              <img src={artist ? artist?.images[1].url : ""} alt="" />
+            </Box>
+          </Fade>
+        </Modal>
         <Box
           component="span"
-          sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.6)' }}
+          sx={{
+            display: 'inline-block', mx: '2px',
+            transform: 'scale(0.6)', backgroundColor: '#242526',
+            color: 'white', padding: '30px 5px 10px 5px',
+            borderRadius: 3, boxShadow: 3
+          }}
         >
-          <Grid container spacing={2} fontSize='60px'>
+          <Grid container spacing={2} fontSize='40px'>
             <Grid item xs={4}>
               <Tooltip title='Follower' placement="left">
                 <PeopleAltIcon sx={{ fontSize: 60 }} />
@@ -63,7 +127,7 @@ export const ArtistComponent = () => {
               {artist ? artist?.popularity : ""}
             </Grid>
             <Grid item xs={4}>
-              <Tooltip title='Album' placement="left">
+              <Tooltip title='Albums' placement="left">
                 <AlbumIcon sx={{ fontSize: 60 }} />
               </Tooltip>
             </Grid>
@@ -72,116 +136,26 @@ export const ArtistComponent = () => {
             </Grid>
           </Grid>
         </Box>
-        {topTrack ? topTrack?.tracks.map((track: any) => { // Hier weiter machen
+      </Stack>
+      {
+        topTrack ? topTrack?.tracks.map((track: any) => { // Hier weiter machen
           return (
-            <div style={{display:'flex'}}>
-              <div>
-                {track ? track?.album.name : ""}
-              </div>
-              <div>
-                {track ? track?.name : ""}
-              </div>
+            <div>
+              <table>
+                <tr>
+                  <th>Album</th>
+                  <th>Top-Track</th>
+                </tr>
+                <tr>
+                  <td>{track ? track?.album.name : ""}</td>
+                  <td>{track ? track?.name : ""}</td>
+                </tr>
+              </table>
             </div>
           )
-        }) : ""}
-        <Grid
-          sx={{ p: 2 }}
-          container
-          direction="row"
-          justifyContent="center"
-          alignItems="flex-start"
-        >
-          <Grid item xs={4}>
-            <Typography>
-              <strong>Tools</strong>
-            </Typography>
-            <Grid
-              sx={{ pt: 4 }}
-              container
-              spacing={2}
-              direction="column"
-              justifyContent="center"
-              alignItems="center"
-              color="text.secondary"
-            >
-              <Grid item xs={2}>
-                <Link color="inherit" underline="none" href="/Lebenslauf">
-                  Künstler mit höchsten Einkommen
-                </Link>
-              </Grid>
-              <Grid item xs={2}>
-                <Link color="inherit" underline="none" href="/Arbeitzeugnis">
-                  Top 100 Künstler
-                </Link>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xs={4}>
-            <Typography>
-              <strong>Informationen</strong>
-            </Typography>
-            <Grid
-              sx={{ pt: 4 }}
-              container
-              spacing={2}
-              direction="column"
-              justifyContent="center"
-              alignItems="center"
-              color="text.secondary"
-            >
-              <Grid item xs={2}>
-                <Link color="inherit" underline="none" href="/preise">
-                  Preise
-                </Link>
-              </Grid>
-              <Grid item xs={2}>
-                <Link color="inherit" underline="none" href="/reviews">
-                  Reviews
-                </Link>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xs={4}>
-            <Typography>
-              <strong>Support</strong>
-            </Typography>
-            <Grid
-              sx={{ pt: 4 }}
-              container
-              spacing={2}
-              direction="column"
-              justifyContent="center"
-              alignItems="center"
-              color="text.secondary"
-            >
-              <Grid item xs={2}>
-                <Link color="inherit" underline="none" href="/faq">
-                  FAQ
-                </Link>
-              </Grid>
-              <Grid item xs={2}>
-                <Link color="inherit" underline="none" href="/kontakt">
-                  Kontakt
-                </Link>
-              </Grid>
-              <Grid item xs={2}>
-                <Link color="inherit" underline="none" href="/agb">
-                  AGB
-                </Link>
-              </Grid>
-              <Grid item xs={2}>
-                <Link color="inherit" underline="none" href="/impressum">
-                  Impressum
-                </Link>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Typography variant="body1">
-          test
-        </Typography>
-      </Stack>
-    </Container>
+        }) : ""
+      }
+    </Container >
   );
 };
 
