f5940723e9b17630ffba5285e41f1720c03e3b83
Moritz Ternes <moritzternes@gmail.com>
2024-03-26 02:18:52 +0100

    frontend: chagned top song component

--- frontend/src/Components/Top20Songs.tsx
+++ frontend/src/Components/Top20Songs.tsx
@@ -1,53 +1,146 @@
-import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import SkipNextIcon from '@mui/icons-material/SkipNext';
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import { Grid, Container } from "@mui/material";
 
-export default function Top20Songs() {
+interface SongCardProps {
+  songName: string;
+  artistName: string;
+  previewSongURL: string;
+}
+
+const SongCard = (props: SongCardProps) => {
   const theme = useTheme();
 
   return (
-    <>
-    <Typography gutterBottom variant="h3" component="div">
-        Die 20 meistgestreamten Songs auf{" "}
-        <Box sx={{ color: "#1DB954" }}>Spotify</Box>
-      </Typography>
-    <Card sx={{ display: 'flex',  width:'400px'}}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <CardContent sx={{ flex: '1 0 auto' }}>
+    <Card sx={{ display: "flex", width: "390px" }}>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <CardContent sx={{ flex: "1 0 auto" }}>
           <Typography component="div" variant="h5">
-            Live From Space
+            {props.songName}
           </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            Mac Miller
+          <Typography
+            variant="subtitle1"
+            color="text.secondary"
+            component="div"
+          >
+            {props.artistName}
           </Typography>
         </CardContent>
-        <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+        <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
           <IconButton aria-label="previous">
-            {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
+            {theme.direction === "rtl" ? (
+              <SkipNextIcon />
+            ) : (
+              <SkipPreviousIcon />
+            )}
           </IconButton>
           <IconButton aria-label="play/pause">
             <PlayArrowIcon sx={{ height: 38, width: 38 }} />
           </IconButton>
           <IconButton aria-label="next">
-            {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
+            {theme.direction === "rtl" ? (
+              <SkipPreviousIcon />
+            ) : (
+              <SkipNextIcon />
+            )}
           </IconButton>
         </Box>
       </Box>
+      <Grid container justifyContent="flex-end">
       <CardMedia
         component="img"
-        sx={{ width: 151 }}
+        sx={{ width: "90%" }}
         image="https://upload.wikimedia.org/wikipedia/en/1/16/Justin_Bieber_-_Changes.png"
         alt="Live from space album cover"
       />
+      </Grid>
     </Card>
-    </>
   );
-}
+};
+
+const songData: SongCardProps[] = [
+  {
+    songName: "Live From Space",
+    artistName: "test",
+    previewSongURL: "test",
+  },
+  {
+    songName: "test",
+    artistName: "Live From Space",
+    previewSongURL: "test",
+  },
+  {
+    songName: "test",
+    artistName: "Live From Space",
+    previewSongURL: "test",
+  },
+  {
+    songName: "test",
+    artistName: "Live From Space",
+    previewSongURL: "test",
+  },
+  {
+    songName: "test",
+    artistName: "Live From Space",
+    previewSongURL: "test",
+  },
+  {
+    songName: "test",
+    artistName: "test",
+    previewSongURL: "test",
+  },
+  {
+    songName: "test",
+    artistName: "test",
+    previewSongURL: "test",
+  },
+  {
+    songName: "test",
+    artistName: "test",
+    previewSongURL: "test",
+  },
+  {
+    songName: "test",
+    artistName: "test",
+    previewSongURL: "test",
+  },
+  {
+    songName: "test",
+    artistName: "test",
+    previewSongURL: "test",
+  }
+];
+
+const Top20Songs = () => {
+  const theme = useTheme();
+
+  return (
+    <Container maxWidth="xl">
+      <Typography gutterBottom variant="h3" component="div">
+        Die 20 meistgestreamten Songs auf{" "}
+        <Box sx={{ color: "#1DB954" }}>Spotify</Box>
+      </Typography>
+      <Grid sx={{ pt: 3 }} container direction={"row"} spacing={1}>
+        {songData.map((data, id) => (
+          <Grid item md={4}>
+            <SongCard
+              artistName={data.artistName}
+              songName={data.songName}
+              previewSongURL={data.previewSongURL}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default Top20Songs;
